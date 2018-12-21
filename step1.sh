8aafb727a5740bb3a7fa6b66727b0cef12b846da
parttimehacker <parttimehacker@gmail.com>
2018-12-21 07:31:34 -0800

    First commit

---
+++ step1.sh
@@ -0,0 +1,28 @@
+#/usr/bin/bash
+#
+# Script: step1.sh
+# Author: parttimehacker 
+# Date:   12/18/2018 
+# 
+# Purpose:fresh install script for Raspberry Pi step 1 of 3 
+# 
+# Notes:
+# 
+sudo apt-get update
+sudo apt-get -y upgrade 
+sudo apt-get -y dist-upgrade
+sudo sync
+echo
+echo "updates, upgrade complete"
+echo
+
+sudo apt-get install avahi-daemon
+sudo systemctl enable avahi-daemon.service
+sudo apt-get -y install netatalk
+sudo sync
+echo
+echo "avihi and netatalk complete"
+echo
+echo "reboot required"
+echo "then step 2"
+
---
+++ step3.sh
@@ -0,0 +1,41 @@
+#/usr/bin/bash
+#
+# Script: step3.sh
+# Author: parttimehacker 
+# Date:   12/18/2018 
+# 
+# Purpose:fresh install script for Raspberry Pi step 3 of 3
+# 
+# Notes:
+# 
+
+mkdir systemd
+cd systemd
+git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
+cd Adafruit_Python_GPIO
+sudo python setup.py install
+cd ..
+sudo sync
+echo 
+echo "GPIO install complete"
+echo 
+
+git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
+cd Adafruit_Python_SSD1306
+sudo python setup.py install
+cd ..
+sudo sync
+echo 
+echo "OLED install complete"
+echo 
+
+git clone https://github.com/adafruit/Adafruit_Python_LED_Backpack.git 
+cd Adafruit_Python_LED_Backpack
+sudo python setup.py install
+cd ..
+sudo sync
+echo 
+echo "Backpack install complete"
+echo
+echo "reboot required"
+
