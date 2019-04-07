8b1f2decae6a2808e5b35a3e64272b25bd39819e
Dave Wilson <parttimehacker@gmail.com>
2019-04-07 09:08:03 -0700

    Update python3-and-flask.sh
    Add fix for python3 rip

--- python3-and-flask.sh
+++ python3-and-flask.sh
@@ -1,8 +1,8 @@
 # development step: Python stuff
 echo "Install Python packages"
-sudo apt-get install -y python-pip
-sudo apt-get install -y build-essential python-dev python-smbus
-sudo apt-get install i2c-tools
+sudo apt-get -y install python-pip
+sudo apt-get -y install build-essential python-dev python-smbus
+sudo apt-get -y install i2c-tools
 sudo apt-get -y install python3-dev python3-pip python3-venv
 sudo -H pip3 install -U pip numpy
 sudo apt-get -y install python3-testresources
@@ -15,7 +15,8 @@ sudo apt-get -y install python-imaging python-pil
 sudo python -m pip install --upgrade pip setuptools wheel
 sudo apt-get -y install git
 sudo pip3 install Pillow
-sudo apt-get install -y libfreetype6-dev libjpeg-dev libopenjp2-7-dev
+sudo apt-get -y install libfreetype6-dev libjpeg-dev libopenjp2-7-dev
+sudo apt-get -y install python3-rpi.gpio
 echo "================================"
 echo "Python package installation complete"
 sudo sync
