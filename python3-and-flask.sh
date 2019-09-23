4293dbc48eb639cf0ef55d96fc935bfcef2fe96d
Dave Wilson <parttimehacker@gmail.com>
2019-09-22 19:14:17 -0700

    Update python3-and-flask.sh
    restore -Get

--- python3-and-flask.sh
+++ python3-and-flask.sh
@@ -1,9 +1,9 @@
 # development step: Python stuff
 echo "Install Python packages"
-sudo apt -y install python-pip
-sudo apt -y install build-essential python-dev python-smbus
-sudo apt -y install i2c-tools
-sudo apt -y install python3-dev python3-pip python3-venv
+sudo apt-get -y install python-pip
+sudo apt-get -y install build-essential python-dev python-smbus
+sudo apt-get -y install i2c-tools
+sudo apt-get -y install python3-dev python3-pip python3-venv
 sudo -H pip3 install -U pip numpy
 sudo apt-get -y install python3-testresources
 sudo pip3 install pylint
@@ -11,12 +11,12 @@ sudo pip3 install flask
 sudo pip3 install requests 
 sudo pip3 install flask-httpauth
 sudo pip3 install paho-mqtt
-sudo apt -y install python-imaging python-pil
+sudo apt-get -y install python-imaging python-pil
 sudo python -m pip install --upgrade pip setuptools wheel
-sudo apt -y install git
+sudo apt-get -y install git
 sudo pip3 install Pillow
-sudo apt -y install libfreetype6-dev libjpeg-dev libopenjp2-7-dev
-sudo apt -y install python3-rpi.gpio
+sudo apt-get -y install libfreetype6-dev libjpeg-dev libopenjp2-7-dev
+sudo apt-get -y install python3-rpi.gpio
 echo "================================"
 echo "Python package installation complete"
 sudo sync
