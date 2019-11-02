3a8a13f8e4ac187f8f0f291735f2105d84284f3c
Dave Wilson <parttimehacker@gmail.com>
2019-11-02 12:45:51 -0700

    Update systemctl.sh
    comments

--- systemctl.sh
+++ systemctl.sh
@@ -1,4 +1,13 @@
+#/usr/bin/bash
+#
+# Script: systemctl.sh
+# Author: parttimehacker 
+# Date:   11/2/2019 
+# 
+# Purpose:fresh install script for systemctl services
+# 
 sudo cp $1.service /lib/systemd/system/$1.service
 sudo chmod 644 /lib/systemd/system/$1.service
 sudo systemctl daemon-reload
 sudo systemctl enable $1.service
+echo "done"
